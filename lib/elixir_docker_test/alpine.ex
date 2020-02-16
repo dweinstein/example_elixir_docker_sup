78782f0182e2381c85874fd08fd7387bdee08624
David Weinstein <dweinst@insitusec.com>
2020-02-16 18:00:02 -0500

    Refactor

--- lib/elixir_docker_test/alpine.ex
+++ lib/elixir_docker_test/alpine.ex
@@ -1,34 +1,45 @@
 defmodule Alpine.Service do
-  @name "alpine"
   use GenServer
   require Logger
 
-  def start_link(_) do
-    GenServer.start_link(__MODULE__, [], name: {:global, :alpine})
+  @mod __MODULE__
+  @defaults %{
+    container_name: "alpine",
+    image_name: "alpine",
+    shell: "/bin/sh"
+  }
+
+  @spec start_link(maybe_improper_list) :: :ignore | {:error, any} | {:ok, pid}
+  def start_link(opts \\ []) when is_list(opts) do
+    GenServer.start_link(@mod, @defaults, name: {:global, @mod})
   end
 
-  def cmd(args \\ []) do
-    GenServer.call({:global, :alpine}, {:cmd, args})
+  def cmd(args) when is_list(args) do
+    GenServer.call({:global, @mod}, {:cmd, args})
+  end
+
+  def cmd(cmd) when is_binary(cmd) do
+    GenServer.call({:global, @mod}, {:cmd, [cmd]})
   end
 
   # Callbacks
 
   @impl true
-  def init(_) do
-    Process.flag(:trap_exit, true)
-    start()
+  def init(opts) do
+    # Process.flag(:trap_exit, true)
+    start(opts)
   end
 
   @impl true
-  def terminate(reason, state) do
+  def terminate(_reason, _state = %{config: opts}) do
     # Do Shutdown Stuff
-    Logger.info("Stopped: #{inspect(stop())}")
+    Logger.info("Stopped: #{inspect(stop(opts.container_name))}")
     :normal
   end
 
   @impl true
-  def handle_call({:cmd, args}, _from, state) do
-    case exec_cmd(args) do
+  def handle_call({:cmd, args}, _from, state = %{config: opts}) do
+    case exec_cmd(opts.container_name, args) do
       {stdio, 0} ->
         {:reply, {:ok, stdio}, state}
 
@@ -37,17 +48,17 @@ defmodule Alpine.Service do
     end
   end
 
-  def start do
-    case run() do
+  def start(opts) do
+    case run(opts.image_name, opts.container_name, opts.shell) do
       {_stdio, 125} ->
         # already running
         # Logger.info("already running")
-        {:ok, %{}}
+        {:ok, %{config: opts}}
 
       {stdio, 0} ->
         # started
         Logger.info("started: #{stdio}")
-        {:ok, %{}}
+        {:ok, %{config: opts}}
 
       {stdio, code} ->
         # other
@@ -55,8 +66,8 @@ defmodule Alpine.Service do
     end
   end
 
-  def exec_cmd(args) do
-    args = ["exec", @name] ++ args
+  defp exec_cmd(container_name, args) do
+    args = ["exec", container_name] ++ args
 
     System.cmd(
       "docker",
@@ -65,18 +76,18 @@ defmodule Alpine.Service do
     )
   end
 
-  def run do
+  defp run(image, container_name, shell \\ "/bin/sh") do
     System.cmd(
       "docker",
-      ["run", "--init", "-d", "-t", "--rm", "--name", @name, "alpine", "/bin/sh"],
+      ["run", "--init", "-d", "-t", "--rm", "--name", container_name, image, shell],
       stderr_to_stdout: true
     )
   end
 
-  def stop do
+  defp stop(container_name) do
     case System.cmd(
            "docker",
-           ["rm", "-f", "alpine"],
+           ["rm", "-f", container_name],
            stderr_to_stdout: true
          ) do
       {stdio, 0} -> {:ok, stdio}
